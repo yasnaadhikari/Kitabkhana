f9b93587ab902b140f7aa4ea212770d8be47b3ab
yasnathedon <yasnaadhikari15@gmail.com>
2022-03-08 12:40:51 +0545

    Database

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from mainapp.models import Post, Replie, UserRating, SaveForLater
+from mainapp.models import AddToCart, Checkout, Post, Replie, UserRating, SaveForLater
 # Register your models here.
 
 admin.site.register(UserRating)
 admin.site.register(SaveForLater)
 admin.site.register(Post)
 admin.site.register(Replie)
+admin.site.register(AddToCart)
+admin.site.register(Checkout)
